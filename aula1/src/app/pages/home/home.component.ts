1aac60ef4c305b660d340afc6787e11f892b87dd
Raissa <raissanavarro66@gmail.com>
2025-02-20 16:51:21 -0300

    16:51 20/02

--- aula1/src/app/pages/home/home.component.ts
+++ aula1/src/app/pages/home/home.component.ts
@@ -13,7 +13,8 @@ export class HomeComponent {
     { imagem: 'https://www.apple.com/v/iphone/home/cb/images/overview/select/iphone_16pro__erw9alves2qa_xlarge.png', nome: 'iPhone 16 Pro', preco: 10.499 },
     { imagem: 'https://www.apple.com/v/iphone/home/cb/images/overview/select/iphone_16__c5bvots96jee_xlarge.png', nome: 'iPhone 16', preco: 7.799 },
     { imagem: 'https://www.apple.com/v/iphone/home/cb/images/overview/select/iphone_16__c5bvots96jee_xlarge.png', nome: 'iPhone 16', preco: 7.799 },
-    { imagem: 'https://www.apple.com/v/iphone/home/cb/images/overview/select/iphone_16pro__erw9alves2qa_xlarge.png', nome: 'iPhone 16 Pro', preco: 10.499 }
+    { imagem: 'https://www.apple.com/v/iphone/home/cb/images/overview/select/iphone_16pro__erw9alves2qa_xlarge.png', nome: 'iPhone 16 Pro', preco: 10.499 },
+    
   ];
   
   //aqui é a definição de uma lista com os produtos
